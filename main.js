0a4d7a50e3c9b7bf6e945e714185025b1608d620
atiq asef <atiqasef19@gmail.com>
2025-04-12 17:23:17 +0600

    almost complete

--- main.js
+++ main.js
@@ -1,48 +1,111 @@
-function loadCategories(){
+function loadCategories() {
     fetch('https://openapi.programming-hero.com/api/levels/all')
-    .then(res=> res.json())
-    .then(data=>displayCategories(data.data))
+        .then(res => res.json())
+        .then(data => displayCategories(data.data))
 }
-function displayCategories(categories){
-    const categoryContainer = document.getElementById('category-container');
-    for(let cat of categories){
+
+const loadCategoryWords = (level) => {
+
+    const url = `https://openapi.programming-hero.com/api/level/${level}`;
+
+
+
+    console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const clickButton = document.getElementById(`btn-${level}`);
+            clickButton.classList.add("active");
+
+
+            displayWords(data.data);
+        })
+}
+
+
+
+document.getElementById("category-container").addEventListener("click", function (event) {
+    if (event.target.classList.contains("btn")) {
+        document.querySelectorAll(".btn").forEach(btn => btn.classList.remove("active-btn"));
+        event.target.classList.add("active-btn");
+    }
+});
+
+
+
+
+
+
+
+
+
+function displayCategories(categories) {
+    const categoryContainer = document.getElementById("category-container");
+    for (let cat of categories) {
         const categoryDiv = document.createElement('div');
-        categoryDiv.innerHTML=`
+        categoryDiv.innerHTML = `
         
-            <button onclick="loadWords(); loadCategoryWords(${cat.level_no})" class="mr-2 btn btn-sm px-3 py-2 text-[#422AD5] border border-[#422AD5] rounded-sm hover:bg-[#422AD5] hover:text-white"> <i class="fa-solid fa-book-open"></i> Lesson -${cat.level_no}</button>
+            <button id="btn-${cat.level_no}" onclick="loadWords(); loadCategoryWords(${cat.level_no})" class="mr-2 btn btn-sm px-3 py-2 text-[#422AD5] border border-[#422AD5] rounded-sm hover:bg-[#422AD5] hover:text-white"> <i class="fa-solid fa-book-open"></i> Lesson -${cat.level_no}</button>
         
         `
         categoryContainer.append(categoryDiv);
     }
 }
-const loadCategoryWords = (level) =>{
 
-    const url = `https://openapi.programming-hero.com/api/level/${level}`;
 
-console.log(url);
-fetch(url).then(res=> res.json()).then(data=> displayWords(data.data))
+function loadWords() {
+    fetch('https://openapi.programming-hero.com/api/level/5')
+        .then(res => res.json())
+        .then(data => displayWords(data.data))
 }
 
 
 
 
-
-function displayWords(words){
+function displayWords(words) {
     const categoryWords = document.getElementById('category-words');
     categoryWords.innerHTML = "";
-    for(let word of words){
+    if (words.length == 0) {
+        categoryWords.innerHTML = `
+        
+
+        <div id="category-words" class="grid grid-cols-3 gap-4 w-[1200px] mx-auto">
+                    <div class="flex flex-col items-center justify-center col-span-3 h-60">
+                        <img src="./assets/alert-error.png" alt="">
+                        <p class="hind-siliguri-regular mb-3 text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                        <h1 class="hind-siliguri-semibold text-4xl">নেক্সট Lesson এ যান</h1>
+                    </div>
+                </div>
+        
+        
+        `;
+        return
+    }
+    for (let word of words) {
         const categoryAllWords = document.createElement('div');
-        categoryAllWords.innerHTML=`
+        categoryAllWords.innerHTML = `
         
              <div class="bg-white w-full rounded-md shadow-md p-2">
             <h1 class="poppins-bold pt-6 pb-4 text-3xl">${word.word}</h1>
             <p class="poppins-regular pb-4 inter text-lg">Meaning /Pronounciation</p>
-            <p class="hind-siliguri-regular pb-15 hind-siliguri-semibold text-3xl">"${word.meaning}/${word.pronunciation}"</p>
-            <div class="flex justify-between px-6 pb-14">
-                <div class="h-14 w-14 bg-[#1a90ff5d] flex justify-center items-center">
-                    <i class="fa-solid fa-circle-info  text-[#374957]"></i>
+            <p class="hind-siliguri-regular pb-15 hind-siliguri-semibold text-3xl">"${word.meaning || "অর্থ নেই"}/${word.pronunciation}"</p>
+            <div  class="flex justify-between px-6 pb-14">
+                <div  class="h-14 w-14 rounded-lg bg-[#1a90ff5d] flex justify-center items-center">
+                   
+             <button class="" onclick="wordDetailsId('${word.id}')"; "word_explain.showModal()"><i   class="fa-solid fa-circle-info  text-[#374957]"></i>
+             
+             </button>
+
+                   <div>
+            
+
+
+        </div>
+
+
+
                 </div>
-                <div class="h-14 w-14 bg-[#1a90ff5d] flex justify-center items-center">
+                <div  class="h-14 w-14 rounded-lg bg-[#1a90ff5d] flex justify-center items-center">
                     <i class="fa-solid fa-volume-high text-[#374957]"></i>
                 </div>
                
@@ -60,3 +123,15 @@ function displayWords(words){
 
 
 loadCategories()
+
+// Smooth Scroll
+
+document.getElementById("faq-btn").addEventListener("click", function () {
+    document.getElementById("faq").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("learn-btn").addEventListener("click", function () {
+    document.getElementById("learn").scrollIntoView({ behavior: "smooth" });
+});
+
+
--- word.js
+++ word.js
@@ -1,6 +1,46 @@
-function loadWords(){
-    fetch('https://openapi.programming-hero.com/api/level/5')
-    .then(res=> res.json())
-    .then(data=> displayWords(data.data))
-}
+const wordDetailsId = (wordId) =>{
+console.log(wordId);
+const url = `https://openapi.programming-hero.com/api/word/${wordId}`;
+fetch(url)
+.then( (res)=> res.json() )
+.then((data) => displayWordDetails(data.data));
+};
 
+const displayWordDetails = (word) =>{
+  console.log(word);
+  document.getElementById('word_explain').showModal();
+  const wordDetails= document.getElementById('word-details');
+  wordDetails.innerHTML=`
+
+        <h3 class="text-3xl text-start font-bold poppins-bold ">${word.word} (<i class="fa-solid fa-microphone-lines"></i> ${word.pronunciation})</h3>
+              <h4 class="text-xl text-start font-bold mt-8 mb-2 poppins-bold ">Meaning</h4>
+              <p class="text-xl text-start mb-8 hind-siliguri-semibold">${word.meaning || "অর্থ পাওয়া যায়নি"} </p>
+
+              <h4 class="text-xl text-start font-bold mt-8 poppins-bold">Example</h4>
+              <p class="text-xl text-start mt-2 poppins-regular">${word.sentence}</p>
+              <p class="mt-10 text-xl text-start hind-siliguri-regular">সমার্থক শব্দ গুলো</p>
+              <div class="flex gap-2 flex-wrap">
+               ${(word.synonyms && word.synonyms.length > 0) ? word.synonyms.map(syn => `
+               <p class="py-3 px-8 bg-[#D7E4EF] text-base poppins-regular rounded-md">${syn}</p>`)
+              .join(''): `<p class=" text-base poppins-regular rounded-md"></p>`
+                  }
+              </div>
+
+
+
+
+
+              <div class="flex gap-2">
+                ${(word.synonyms || []).map(syn => `
+                  <p class="py-3 px-8 bg-[#D7E4EF] text-base poppins-regular rounded-md">${syn}</p>
+                `).join('')}
+              </div>
+              <div class="text-start ">
+  <button onclick="document.getElementById('word_explain').close();" class="bg-[#422AD5]  px-8 py-2 mt-12 text-white rounded-lg hind-siliguri-regular">
+    Close
+  </button>
+</div>
+
+  
+  `
+}
